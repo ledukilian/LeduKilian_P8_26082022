09db6621413ed23ab9772ffbd75ed10bb5259de5
ledukilian <ledu.kilian@gmail.com>
2022-09-29 19:18:28 +0200

    Progress on update to 5.1

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends Controller
+class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
